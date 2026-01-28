2bed56bcc2616e44cd0fcf9b4f8ef0860275d292
txomin <txomin.sirera@clarity.ai>
2026-01-28 12:17:25 +0100

    update spectral ruleset URL

--- .spectral.mjs
+++ .spectral.mjs
@@ -1,2 +1,2 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE2Njc5Mw/spectral.js?branch_slug=master&token=99510663-b629-4b3c-95fc-64ffb1ef9cf6";
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwNDM0NA/spectral.js?branch_slug=main&token=99510663-b629-4b3c-95fc-64ffb1ef9cf6";
 export default { extends: ruleset };
