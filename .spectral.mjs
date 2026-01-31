44d74a6769ee0d897ff96b52a468b9af59941a6f
txomin <txomin.sirera@clarity.ai>
2026-01-31 21:39:34 +0100

    remove unnecessary files

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwNDM0NA/spectral.js?branch_slug=main&token=99510663-b629-4b3c-95fc-64ffb1ef9cf6";
-export default { extends: ruleset };
